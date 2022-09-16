756ca5bebcea1e4ba5a81fd04a8807a134c30a71
JaretWright <jaret.wright@georgiancollege.ca>
2022-09-16 12:53:40 -0400

    Lesson 2 - Started building a create beer view.

---
+++ src/main/java/com/example/f22comp1011lhw1/CreateBeerController.java
@@ -0,0 +1,4 @@
+package com.example.f22comp1011lhw1;
+
+public class CreateBeerController {
+}
---
+++ src/main/java/com/example/f22comp1011lhw1/Main.java
@@ -0,0 +1,24 @@
+package com.example.f22comp1011lhw1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.TreeMap;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("create-beer-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Wright Way Beer Co.");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
